91ca1b461f28989b6c682aafc792d5f4bcc7460d
anaclaralmz <ana.zaidan@sou.inteli.edu.br>
2024-06-12 02:44:15 -0300

    feat: implementacao coleta de metricas

---
+++ BlazorApp1/BlazorApp1/ContosoMetrics.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics.Metrics;
+
+namespace BlazorApp1;
+
+public class ContosoMetrics
+{
+    private readonly Counter<int> _productSoldCounter;
+
+    public ContosoMetrics(IMeterFactory meterFactory)
+    {
+        var meter = meterFactory.Create("Contoso.Web");
+        _productSoldCounter = meter.CreateCounter<int>("contoso.product.sold");
+    }
+
+    public void ProductSold(string productName, int quantity)
+    {
+        _productSoldCounter.Add(quantity,
+            new KeyValuePair<string, object?>("contoso.product.name", productName));
+    }
+}
---
+++ BlazorApp1/BlazorApp1/SaleModel.cs
@@ -0,0 +1,7 @@
+namespace BlazorApp1;
+
+public class SaleModel
+{
+    public string ProductName { get; set; }
+    public int QuantitySold { get; set; }
+}
